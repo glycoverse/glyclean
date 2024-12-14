724a7b65d37622d963d090f1b1864dcf9dc2a7ed
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-12-14 14:27:25 +0800

    feat: add `bpca_impute()`, `ppca_impute()`, and `svd_impute()`

--- R/impute.R
+++ R/impute.R
@@ -45,6 +45,7 @@ half_sample_min_impute <- function(exp) {
 
 #' Sample-wise KNN Imputation
 #'
+#' A wrapper around the [impute::impute.knn()].
 #' Impute missing values with values from the k-nearest neighbors of the
 #' corresponding sample.
 #' If there are strong patterns among the samples (such as group clustering
@@ -66,6 +67,7 @@ sw_knn_impute <- function(exp, k = 5, by = NULL, ...) {
 
 #' Feature-wise KNN Imputation
 #'
+#' A wrapper around the [impute::impute.knn()].
 #' Impute missing values with values from the k-nearest neighbors of the
 #' corresponding feature.
 #'
@@ -79,6 +81,65 @@ sw_knn_impute <- function(exp, k = 5, by = NULL, ...) {
 #' @export
 fw_knn_impute <- function(exp, k = 5, by = NULL, ...) {
   .update_expr_mat(exp, .fw_knn_impute, k = k, by = by, ...)
+}
+
+
+#' BPCA Imputation
+#'
+#' A wrapper around the [pcaMethods::pca()].
+#' Impute missing values using Bayesian principal component analysis (BPCA).
+#' BPCA combines an EM approach for PCA with a Bayesian model.
+#' In standard PCA data far from the training set but close to the principal
+#' subspace may have the same reconstruction error.
+#' BPCA defines a likelihood function such that the likelihood for data far from
+#' the training set is much lower, even if they are close to the principal subspace.
+#'
+#' @param exp An expression matrix.
+#' @param by A grouping variable to consider when imputing missing values.
+#' This variable should be a column in the sample information table.
+#' @param ... Additional arguments to pass to `pcaMethods::pca()`.
+#'
+#' @return An expression matrix with missing values imputed.
+#' @export
+bpca_impute <- function(exp, by = NULL, ...) {
+  .update_expr_mat(exp, .bpca_impute, by = by, ...)
+}
+
+
+#' PPCA Imputation
+#'
+#' A wrapper around the [pcaMethods::pca()].
+#' Impute missing values using probabilistic principal component analysis (PPCA).
+#' PPCA allows to perform PCA on incomplete data and may be used for missing value estimation.
+#'
+#' @param exp An expression matrix.
+#' @param by A grouping variable to consider when imputing missing values.
+#' This variable should be a column in the sample information table.
+#' @param ... Additional arguments to pass to `pcaMethods::pca()`.
+#'
+#' @return An expression matrix with missing values imputed.
+#' @export
+ppca_impute <- function(exp, by = NULL, ...) {
+  .update_expr_mat(exp, .ppca_impute, by = by, ...)
+}
+
+
+#' SVD Imputation
+#'
+#' A wrapper around the [pcaMethods::pca()].
+#' Impute missing values using singular value decomposition (SVD) imputation.
+#' SVD is a matrix factorization technique that factors a matrix into three matrices:
+#' U, Σ, and V. SVD is used to find the best lower rank approximation of the original matrix.
+#'
+#' @param exp An expression matrix.
+#' @param by A grouping variable to consider when imputing missing values.
+#' This variable should be a column in the sample information table.
+#' @param ... Additional arguments to pass to `pcaMethods::pca()`.
+#'
+#' @return An expression matrix with missing values imputed.
+#' @export
+svd_impute <- function(exp, by = NULL, ...) {
+  .update_expr_mat(exp, .svd_impute, by = by, ...)
 }
 
 
@@ -103,10 +164,39 @@ fw_knn_impute <- function(exp, k = 5, by = NULL, ...) {
 
 
 .sw_knn_impute <- function(mat, k = 5, ...) {
-  impute::impute.knn(mat, k = k, ...)$data
+  normed <- log2(mat)
+  normed <- impute::impute.knn(normed, k = k, ...)$data
+  2 ^ normed
 }
 
 
 .fw_knn_impute <- function(mat, k = 5, ...) {
-  t(impute::impute.knn(t(mat), k = k, ...)$data)
+  normed <- log2(mat)
+  normed <- t(impute::impute.knn(t(normed), k = k, ...)$data)
+  2 ^ normed
+}
+
+
+.bpca_impute <- function(mat, ...) {
+  .pca_methods_wrapper(mat, "bpca", ...)
+}
+
+
+.ppca_impute <- function(mat, ...) {
+  .pca_methods_wrapper(mat, "ppca", ...)
+}
+
+
+.svd_impute <- function(mat, ...) {
+  .pca_methods_wrapper(mat, "svdImpute", ...)
+}
+
+
+.pca_methods_wrapper <- function(mat, method, ...) {
+  normed <- log2(mat)
+  normed <- withr::with_output_sink(
+    tempfile(),
+    pcaMethods::pca(normed, nPcs = 5, method = method, ...)@completeObs
+  )
+  2 ^ normed
 }
--- tests/testthat/test-impute.R
+++ tests/testthat/test-impute.R
@@ -1,3 +1,26 @@
+missing_exp_3_3 <- function() {
+  exp <- simple_exp(3, 3)
+  old_expr_mat <- exp$expr_mat
+  new_expr_mat <- matrix(
+    c(1, 1, NA,
+      NA, 2, 4,
+      1, 3, 6),
+    nrow = 3, byrow = TRUE
+  )
+  colnames(new_expr_mat) <- colnames(old_expr_mat)
+  rownames(new_expr_mat) <- rownames(old_expr_mat)
+  exp$expr_mat <- new_expr_mat
+  exp
+}
+
+
+missing_exp_10_10 <- function() {
+  exp <- simple_exp(10, 10)
+  exp$expr_mat[c(1, 5, 29, 68, 45, 100)] <- NA
+  exp
+}
+
+
 test_that("zero_impute works", {
   exp <- simple_exp(3, 3)
   exp$expr_mat[1, 1] <- NA
@@ -7,92 +30,70 @@ test_that("zero_impute works", {
 
 
 test_that("sample_min_impute works", {
-  exp <- simple_exp(3, 3)
-  old_expr_mat <- exp$expr_mat
-  new_expr_mat <- matrix(
-    c(NA, 1, NA,
-      2, 2, 4,
-      NA, 3, 6),
-    nrow = 3, byrow = TRUE
-  )
-  colnames(new_expr_mat) <- colnames(old_expr_mat)
-  rownames(new_expr_mat) <- rownames(old_expr_mat)
-  exp$expr_mat <- new_expr_mat
-
-  exp <- sample_min_impute(exp)
-
+  old_exp <- missing_exp_3_3()
+  exp <- sample_min_impute(old_exp)
   expected <- matrix(
-    c(2, 1, 4,
-      2, 2, 4,
-      2, 3, 6),
+    c(1, 1, 4,
+      1, 2, 4,
+      1, 3, 6),
     nrow = 3, byrow = TRUE
   )
-  colnames(expected) <- colnames(old_expr_mat)
-  rownames(expected) <- rownames(old_expr_mat)
+  colnames(expected) <- colnames(old_exp$expr_mat)
+  rownames(expected) <- rownames(old_exp$expr_mat)
   expect_equal(exp$expr_mat, expected)
 })
 
 
 test_that("half_sample_min_impute works", {
-  exp <- simple_exp(3, 3)
-  old_expr_mat <- exp$expr_mat
-  new_expr_mat <- matrix(
-    c(NA, 1, NA,
-      2, 2, 4,
-      NA, 3, 6),
-    nrow = 3, byrow = TRUE
-  )
-  colnames(new_expr_mat) <- colnames(old_expr_mat)
-  rownames(new_expr_mat) <- rownames(old_expr_mat)
-  exp$expr_mat <- new_expr_mat
-
-  exp <- half_sample_min_impute(exp)
-
+  old_exp <- missing_exp_3_3()
+  exp <- half_sample_min_impute(old_exp)
   expected <- matrix(
     c(1, 1, 2,
-      2, 2, 4,
+      0.5, 2, 4,
       1, 3, 6),
     nrow = 3, byrow = TRUE
   )
-  colnames(expected) <- colnames(old_expr_mat)
-  rownames(expected) <- rownames(old_expr_mat)
+  colnames(expected) <- colnames(old_exp$expr_mat)
+  rownames(expected) <- rownames(old_exp$expr_mat)
   expect_equal(exp$expr_mat, expected)
 })
 
 
 test_that("sw_knn_impute works", {
-  exp <- simple_exp(3, 3)
-  old_expr_mat <- exp$expr_mat
-  new_expr_mat <- matrix(
-    c(1, 1, NA,
-      NA, 2, 4,
-      1, 3, 6),
-    nrow = 3, byrow = TRUE
-  )
-  colnames(new_expr_mat) <- colnames(old_expr_mat)
-  rownames(new_expr_mat) <- rownames(old_expr_mat)
-  exp$expr_mat <- new_expr_mat
+  exp <- missing_exp_10_10()
+  exp <- sw_knn_impute(exp, k = 5)
 
-  exp <- sw_knn_impute(exp, k = 1)
-
-  expect_snapshot(exp$expr_mat)
+  expect_snapshot(round(exp$expr_mat, 4))
 })
 
 
 test_that("fw_knn_impute works", {
-  exp <- simple_exp(3, 3)
-  old_expr_mat <- exp$expr_mat
-  new_expr_mat <- matrix(
-    c(1, 1, NA,
-      NA, 2, 4,
-      1, 3, 6),
-    nrow = 3, byrow = TRUE
-  )
-  colnames(new_expr_mat) <- colnames(old_expr_mat)
-  rownames(new_expr_mat) <- rownames(old_expr_mat)
-  exp$expr_mat <- new_expr_mat
+  exp <- missing_exp_10_10()
+  exp <- fw_knn_impute(exp, k = 5)
 
-  exp <- fw_knn_impute(exp, k = 1)
+  expect_snapshot(round(exp$expr_mat, 4))
+})
 
-  expect_snapshot(exp$expr_mat)
+
+test_that("bpca_impute works", {
+  exp <- missing_exp_10_10()
+  exp <- bpca_impute(exp)
+
+  expect_snapshot(round(exp$expr_mat, 4))
+})
+
+
+# test_that("ppca_impute works", {
+#   exp <- missing_exp_10_10()
+#   exp <- ppca_impute(exp)
+#
+#   expect_snapshot(round(exp$expr_mat, 4))
+# })
+
+
+test_that("svd_impute works", {
+  exp <- missing_exp_10_10()
+  exp <- svd_impute(exp)
+
+  expect_snapshot(round(exp$expr_mat, 4))
 })
