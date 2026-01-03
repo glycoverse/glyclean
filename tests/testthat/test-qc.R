fa3389875c2afa79360b60e8301e7a230dd0965d
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-01-03 10:53:11 +0800

    test: add test to plot_missing_heatmap

--- tests/testthat/test-qc.R
+++ tests/testthat/test-qc.R
@@ -1,7 +1,15 @@
-test_that("plot_missing_bar orders samples by missing proportion", {
-  skip_on_ci()
-  skip_on_cran()
+skip_on_ci()
+skip_on_cran()
 
+test_that("plot_missing_heatmap works", {
+  test_exp <- simple_exp(3, 3)
+  test_exp$expr_mat[1, 1] <- NA
+  test_exp$expr_mat[1:2, 2] <- NA
+
+  vdiffr::expect_doppelganger("plot_missing_heatmap", plot_missing_heatmap(test_exp))
+})
+
+test_that("plot_missing_bar orders samples by missing proportion", {
   test_exp <- simple_exp(3, 3)
   test_exp$expr_mat[1, 1] <- NA
   test_exp$expr_mat[1:2, 2] <- NA
